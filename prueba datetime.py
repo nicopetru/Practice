d4576ade445427f5799f6aab63454378722b3e83
Nico <pinganico@gmail.com>
2020-09-27 13:16:52 -0300

    septiembre

--- prueba datetime.py
+++ prueba datetime.py
@@ -1,10 +1,18 @@
 import datetime
 
 reloj = datetime.datetime.now()
-#reloj1 = datetime.now()
+hoy = datetime.date.today()
 
 print(reloj)
-#print(reloj1)
+print("hoy",hoy)
 
+nico = datetime.date(1986,2,23) # variable nico con fecha 23-2-86
+print(nico)
+print("año:",nico.year,"mes:",nico.month,"dia:",nico.day)
 
+def edad(x):
+    years = hoy.year - x.year - ((hoy.month, hoy.day) < (x.month, x.day))
+    print((hoy.month, hoy.day) < (x.month, x.day))
+    return years
 
+print("edad",edad(nico))
